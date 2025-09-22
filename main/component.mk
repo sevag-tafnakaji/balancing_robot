2118bc784a9ee40217c336a72af9da66abfb8162
Sevag Tafnakaji <sevag.tafnakaji@gmail.com>
2025-09-22 10:16:40 +0200

    Remove websocket related files

--- main/component.mk
+++ main/component.mk
@@ -2,7 +2,3 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-COMPONENT_EMBED_TXTFILES := resources/ca.pem
-COMPONENT_EMBED_TXTFILES += resources/server.pem
-COMPONENT_EMBED_TXTFILES += resources/server.key
--- main/main.c
+++ main/main.c
@@ -1,20 +1,11 @@
 #include "mpu6050.c"
-#include "webpage.c"
 
 static const char *main_tag = "main";
 
 void app_main(void)
 {
+
     ESP_LOGI(main_tag, "Beginning MPU6050 reading task");
     // start i2c task
     xTaskCreate(i2c_task_example, "i2c_task_example", 2048, NULL, 10, NULL);
-
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    ESP_ERROR_CHECK(example_connect());
-
-    ESP_LOGI(main_tag, "Beginning websocket task");
-    xTaskCreate(&openssl_server_task, "openssl_server", 8192, NULL, 15, NULL);
 }
